704a9f25bf381068b153dd85754f472d883c3954
majobodo <majobodo@web.de>
2023-01-23 13:35:18 +0100

    Update __init__ files

--- applefy/detections/__init__.py
+++ applefy/detections/__init__.py
@@ -1,8 +1,4 @@
 # Most important functions for contrast calculation
 from applefy.detections.preparation import create_and_save_configs
-from applefy.detections.execution import collect_all_data_setup_configs, \
-    add_fake_planets
-from applefy.detections.evaluation import read_and_sort_results, \
-    estimate_stellar_flux, compute_throughput_table, compute_contrast_curve, \
-    compute_detection_confidence, compute_contrast_map, \
-    compute_contrast_from_map, ContrastResult
+from applefy.detections.execution import add_fake_planets
+from applefy.detections.contrast import Contrast, DataReductionInterface
--- applefy/utils/__init__.py
+++ applefy/utils/__init__.py
@@ -7,3 +7,4 @@ from applefy.utils.aperture_positions import get_number_of_apertures, \
     estimate_aperture_positions, estimate_reference_positions
 
 from applefy.utils.general import center_subpixel
+from applefy.utils.data_handling import read_apples_with_apples_root
