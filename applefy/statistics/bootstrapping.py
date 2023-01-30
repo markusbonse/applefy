76fd48714806ca0f7098cc6239dfa620f6bb40cd
majobodo <majobodo@web.de>
2023-01-30 13:59:25 +0100

    tau vs t bug fix in restore of lookup tables

--- applefy/statistics/bootstrapping.py
+++ applefy/statistics/bootstrapping.py
@@ -129,8 +129,9 @@ class BootstrapTest(TTest):
 
         # convert t and fpf lists to np.arrays
         lookups_new = dict()
+        # TODO change tau once we have a new table
         for key, values in json_lookups.items():
-            tmp_dict = {"t": np.array(values["t"]),
+            tmp_dict = {"t": np.array(values["tau"]),
                         "fpf": np.array(values["fpf"])}
             lookups_new[int(key)] = tmp_dict
 
