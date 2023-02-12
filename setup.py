66bd073e94b1b9bb92e848c5fc62327cf36c9fb8
majobodo <majobodo@web.de>
2023-02-12 22:50:23 +0100

    Change pynpoint to git dependency

--- setup.py
+++ setup.py
@@ -63,6 +63,10 @@ setup(
         "fast_sort": ["parallel_sort==0.0.3", ],
         "plotting": ["seaborn", "matplotlib", "bokeh>=3.0.3"],
     },
+    dependency_links=[
+        # Make sure to include the `#egg` portion so the `install_requires` recognizes the package
+        'git+ssh://git@github.com:markusbonse/PynPoint.git#egg=pynpoint-0.10.1'
+    ],
     packages=find_packages(include=['applefy',
                                     'applefy.*']),
     zip_safe=False,
