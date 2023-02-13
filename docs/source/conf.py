3e894f8316969b91f4c0bf4466e64b9106175efa
majobodo <majobodo@web.de>
2023-02-13 20:21:06 +0100

    Update version number to first beta version of the package.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2023, Markus Johannes Bonse'
 author = 'Markus Johannes Bonse'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.2'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="applefy",
-    version="0.0.2",
+    version="0.1.0",
     description=(
         "applefy: A library to compute detection limits for high contrast"
         " imaging of exoplanets"
