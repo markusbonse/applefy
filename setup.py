01bb02b84d283c4574948bb83c84f8addd54affb
majobodo <majobodo@web.de>
2023-09-18 15:55:37 +0200

    Change the version.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="applefy",
-    version="0.1.1",
+    version="0.1.2",
     description=(
         "applefy: A library to compute detection limits for high contrast"
         " imaging of exoplanets"
